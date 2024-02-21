7a45d6b27d213c69bdee8d629b9cd5c4a2eaa651
FnHafiz <farhanhafiz55@gmail.com>
2024-02-21 11:35:15 +0700

    feat: add handling encrypt-decrypt

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,75 @@
 import { useState } from 'react'
 import './App.css'
+import {VigenereEncrypt, VigenereDecrypt} from './utils/VigenereChiper.js'
+import {AutoKeyVigenereEncrypt, AutoKeyVigenereDecrypt} from './utils/AutoKeyVigenereChiper.js'
+import {ExtendedVigenereEncrypt, ExtendedVigenereDecrypt} from './utils/ExtendedVigenereChiper.js'
+import {PlayfairEncrypt, PlayfairDecrypt} from './utils/PlayfairChiper.js'
 
 function App() {
   const [selectedOption, setSelectedOption] = useState('text')
+  const [inputText, setInputText] = useState('')
+  const [selectedChiper, setSelectedChiper] = useState('vignere')
+  const [key, setKey] = useState('')
+  const [outputText, setOutputText] = useState('')
 
   const handleOptionChange = (e) => {
     setSelectedOption(e.target.value)
+  }
+
+  const handleInputChange = (e) => {
+    setInputText(e.target.value)
+  }
+
+  const handleChiperChange = (e) => {
+    setSelectedChiper(e.target.value)
+  }
+
+  const handleKeyChange = (e) => {
+    setKey(e.target.value)
+  }
+
+  const handleEncryptClick = () => {
+    console.log("Encrypting", inputText, key)
+
+    switch(selectedChiper) {
+      case 'vignere':
+        setOutputText(VigenereEncrypt(inputText, key))
+        break
+      case 'auto-key-vignere':
+        setOutputText(AutoKeyVigenereEncrypt(inputText, key))
+        break
+      case 'extended-vignere':
+        setOutputText(ExtendedVigenereEncrypt(inputText, key))
+        break
+      case 'playfair-vignere':
+        setOutputText(PlayfairEncrypt(inputText, key))
+        break
+      default:
+        setOutputText(VigenereEncrypt(inputText, key))
+    }
+    console.log("result : ", outputText)
+  }
+
+  const handleDecryptClick = () => {
+    console.log("Decrypting", inputText, key)
+
+    switch(selectedChiper) {
+      case 'vignere':
+        setOutputText(VigenereDecrypt(inputText, key))
+        break
+      case 'auto-key-vignere':
+        setOutputText(AutoKeyVigenereDecrypt(inputText, key))
+        break
+      case 'extended-vignere':
+        setOutputText(ExtendedVigenereDecrypt(inputText, key))
+        break
+      case 'playfair-vignere':
+        setOutputText(PlayfairDecrypt(inputText, key))
+        break
+      default:
+        setOutputText(VigenereDecrypt(inputText, key))
+    }
+    console.log("result : ", outputText)
   }
 
   return (
@@ -25,11 +89,11 @@ function App() {
         <div className='content'>
           <label className='input-text'>
             Text:
-            <textarea></textarea>
+            <textarea value={inputText} onChange={handleInputChange}></textarea>
           </label>
           <label className='input-type'>
             Chiper Type : 
-            <select>
+            <select value={selectedChiper} onChange={handleChiperChange}>
               <option value="vignere">Vigenere Chiper</option>
               <option value="auto-key-vignere">Auto-Key Vignere Chiper</option>
               <option value="extended-vignere">Extended Vignere Chiper</option>
@@ -40,15 +104,18 @@ function App() {
           </label>
           <label className='input-key'>
             Key:
-            <input type="text" />
+            <input type="text" value={key} onChange={handleKeyChange}/>
           </label>
           <div className='buttons'>
-            <button>Encrypt</button>
-            <button>Decrypt</button>
+            <button onClick={handleEncryptClick}>Encrypt</button>
+            <button onClick={handleDecryptClick}>Decrypt</button>
           </div>
           <div className='output'>
             <label>
-              Output:
+              Input: {inputText}
+            </label>
+            <label>
+              Output: {outputText}
             </label>
           </div>
         </div>
