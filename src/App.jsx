c15cfdae185bc246c0ff80f72deb3f954a587493
FnHafiz <farhanhafiz55@gmail.com>
2024-02-21 10:58:36 +0700

    feat: add front-end for input text

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,72 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [selectedOption, setSelectedOption] = useState('text')
+
+  const handleOptionChange = (e) => {
+    setSelectedOption(e.target.value)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className='title'>
+      <h1>Encrypt-Decrypt Website</h1>
+    </div>
+    <div className='content'>
+      <label className='input-type'>
+        Input Type : 
+        <select value={selectedOption} onChange={handleOptionChange}>
+          <option value="text">Text</option>
+          <option value="file">File</option>
+        </select>
+      </label>
+      {selectedOption === 'text' ? (
+        <div className='content'>
+          <label className='input-text'>
+            Text:
+            <textarea></textarea>
+          </label>
+          <label className='input-type'>
+            Chiper Type : 
+            <select>
+              <option value="vignere">Vigenere Chiper</option>
+              <option value="auto-key-vignere">Auto-Key Vignere Chiper</option>
+              <option value="extended-vignere">Extended Vignere Chiper</option>
+              <option value="playfair-vignere">Playfair Chiper</option>
+              <option value="affine-vignere">Affine Chiper</option>
+              <option value="hill-vignere">Hill Chiper</option>
+            </select>
+          </label>
+          <label className='input-key'>
+            Key:
+            <input type="text" />
+          </label>
+          <div className='buttons'>
+            <button>Encrypt</button>
+            <button>Decrypt</button>
+          </div>
+          <div className='output'>
+            <label>
+              Output:
+            </label>
+          </div>
+        </div>
+      ) : (
+        <div className='content'>
+          <label>
+            File:
+            <input type="file" />
+          </label>
+          <label>
+            Key:
+            <input type="text" />
+          </label>
+          <button>Encrypt</button>
+          <button>Decrypt</button>
+        </div>
+      )}
+    </div>
     </>
   )
 }
